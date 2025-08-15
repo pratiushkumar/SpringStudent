d3ec4977ab32c939888bd78b9775f2b166967752
pratiushkumar <128065429+pratiushkumar@users.noreply.github.com>
2025-08-15 23:40:29 +0530

    MyService

---
+++ src/com/example/MyService.java
@@ -0,0 +1,22 @@
+package com.example;
+
+
+import java.util.Map;
+
+//public class MyService {
+//
+//    private final Map<String, String> myMap; // constructor-injected
+//
+//
+//      // field-injected
+//
+//    public MyService(Map<String, String> myMap) {
+//        this.myMap = myMap;
+//    }
+//
+//    public void doSomething() {
+//        System.out.println(myMap);
+//        anotherService.help();
+//    }
+//}
+
